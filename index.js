a3ed54cc4fa6842afe9dbcd757628ab42166dc9e
itdevmasterteam <itdevmasterteam@gmail.com>
2021-10-18 17:02:20 +0530

    express app created

---
+++ index.js
@@ -0,0 +1,10 @@
+//importing express
+const express = require('express');
+//invoking express function
+const app = express();
+
+
+//listeng to a port
+app.listen(5000, () => {
+    console.log('server listenig at port ');
+})
